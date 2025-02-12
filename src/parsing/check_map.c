97cd3f7a5f6ff953a7e7a57e9dbd45d5972ba910
MAELLE <gbk.maelle@outlook.fr>
2025-02-12 13:21:34 +0100

    save

--- src/parsing/check_map.c
+++ src/parsing/check_map.c
@@ -6,7 +6,7 @@
 /*   By: magrabko <magrabko@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 17:11:08 by magrabko          #+#    #+#             */
-/*   Updated: 2025/02/12 10:03:00 by magrabko         ###   ########.fr       */
+/*   Updated: 2025/02/12 13:20:41 by magrabko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -96,12 +96,11 @@ char	**add_to_map(t_data *data, char *line)
 		while (data->temp->map_check[i])
 		{
 			new_map[i] = ft_strdup(data->temp->map_check[i]);
-			free_ptr((void **)&data->temp->map_check[i]);
 			check_alloc(new_map[i], data);
 			i++;
 		}
-		free_ptr((void **)&data->temp->map_check);
 	}
+	free_tab(&data->temp->map_check);
 	new_map[data->temp->y - 1] = line;
 	new_map[data->temp->y] = NULL;
 	return (new_map);
@@ -114,7 +113,7 @@ char	**add_to_map(t_data *data, char *line)
 **
 **	temp->line == NULL --> true when file read ends.
 */
-int	fill_map(t_data *data)
+int	fill_map_check(t_data *data)
 {
 	while (data->temp->fd_map > 0)
 	{
@@ -127,7 +126,8 @@ int	fill_map(t_data *data)
 		if (!is_line_empty(data->temp->line))
 		{
 			data->temp->y++;
-			data->temp->map_check = add_to_map(data, ft_strtrim(data->temp->line, "\n"));
+			data->temp->map_check = add_to_map(data,
+					ft_strtrim(data->temp->line, "\n"));
 		}
 		free_ptr((void **)&data->temp->line);
 	}
@@ -136,57 +136,123 @@ int	fill_map(t_data *data)
 	return (1);
 }
 
-int	add_rgb(t_data *data, char **rgb)
+int	add_rgb(t_data *data, char *info, int i)
 {
-	
-}
-
-int	add_to_identifier(t_data *data, char *temp, int i)
-{
-	char	*id;
 	char	**rgb;
 
-	id = ft_strdup(temp);
-	check_alloc(id, data);
-	if (access(id, O_RDONLY))
-		return (perror("indentifier"), 0);
-	if (i == 0)
-		data->north = id;
-	else if (i == 1)
-		data->south = id;
-	else if (i == 2)
-		data->west = id;
-	else if (i == 3)
-		data->east = id;
-	else
+	rgb = ft_split(info, ',');
+	free_ptr((void **)&info);
+	check_alloc(rgb, data);
+	if (rgb[3] != NULL)
+		return (0);
+	if (i == 4)
 	{
-		rgb = ft_split(temp, ',');
-		check_alloc(rgb, data);
-		add_rgb(data, rgb);
+		data->f_color[0] = ft_atoi(rgb[0]);
+		data->f_color[1] = ft_atoi(rgb[1]);
+		data->f_color[2] = ft_atoi(rgb[2]);
 	}
+	else if (i == 5)
+	{
+		data->c_color[0] = ft_atoi(rgb[0]);
+		data->c_color[1] = ft_atoi(rgb[1]);
+		data->c_color[2] = ft_atoi(rgb[2]);
+	}
+	return (1);
+}
+
+char	*get_id_info(t_data *data, char *temp, int *index)
+{
+	char	*info;
+	int		end;
+
+	end = 0;
+	while (temp[end] && !search_set(temp[end], ALL_SPACES))
+		end++;
+	info = ft_strndup(temp, end);
+	check_alloc(info, data);
+	*index = end;
+	return (info);
+}
+
+char	*pass_spaces(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] && search_set(str[i], ALL_SPACES))
+		i++;
+	return (&str[i]);
+}
+
+int	set_elements(t_data *data, char *temp, int i, int index)
+{
+	char	*info;
+
+	info = get_id_info(data, temp, &index);
+	temp += index;
+	temp = pass_spaces(temp);
+	if (temp[0])
+		return (0);
+	if (i < 4)
+	{
+		if (access(info, O_RDONLY))
+			return (perror("indentifier"), 0);
+		if (i == 0)
+			data->north = info;
+		else if (i == 1)
+			data->south = info;
+		else if (i == 2)
+			data->west = info;
+		else if (i == 3)
+			data->east = info;
+	}
+	else
+		if (!add_rgb(data, info, i))
+			return (0);
 	return (1);
 }
 
 char	*get_identifier(char *line, char *temp, int i)
 {
-	int	len;
+	int	n;
 
-	len = ft_strlen(line);
+	n = ft_strlen(line);
 	if (i == 0)
-		temp = ft_strnstr(line, "NO", len);
+		temp = ft_strnstr(line, "NO", n);
 	else if (i == 1)
-		temp = ft_strnstr(line, "SO", len);
+		temp = ft_strnstr(line, "SO", n);
 	else if (i == 2)
-		temp = ft_strnstr(line, "WE", len);
+		temp = ft_strnstr(line, "WE", n);
 	else if (i == 3)
-		temp = ft_strnstr(line, "EA", len);
+		temp = ft_strnstr(line, "EA", n);
 	else if (i == 4)
-		temp = ft_strnstr(line, "F", len);
+		temp = ft_strnstr(line, "F", n);
 	else if (i == 5)
-		temp = ft_strnstr(line, "C", len);
-	if (temp == NULL)
+		temp = ft_strnstr(line, "C", n);
+	if (i < 4)
+		n = 2;
+	else
+		n = 1;
+	if (temp == NULL || !search_set(temp[n], ALL_SPACES))
 		return (NULL);
-	return (temp);
+	return (&temp[n]);
+}
+
+void	load_map(t_data *data, int i)
+{
+	int	j;
+
+	data->map = malloc(sizeof(char *) * ((data->temp->y - i) + 1));
+	check_alloc(data->map, data);
+	j = 0;
+	while (data->temp->map_check[i])
+	{
+		data->map[j] = ft_strdup(data->temp->map_check[i]);
+		check_alloc(data->map[j], data);
+		j++;
+		i++;
+	}
+	data->map[j] = NULL;
 }
 
 int	check_identifier(t_data *data, int i)
@@ -194,26 +260,30 @@ int	check_identifier(t_data *data, int i)
 	char	*temp;
 
 	temp = NULL;
-	if (!fill_map(data))
+	if (!fill_map_check(data))
 		return (0);
 	while (i < 6)
 	{
 		temp = get_identifier(data->temp->map_check[i], temp, i);
 		if (!temp)
 			return (0);
-		if (!add_to_identifier(data, temp, i))
+		temp = pass_spaces(temp);
+		if (!temp[0])
+			return (0);
+		if (!set_elements(data, temp, i, 0))
 			return (0);
 		i++;
 	}
+	load_map(data, i);
 	return (1);
 }
 
 int	is_map_valid(t_data *data)
 {
- 	if (!check_identifier(data, 0))
+	if (!check_identifier(data, 0))
 		return (ft_printf_fd(2, "Error: Invalid map.\n"), 0);
-/*	if (check_walls(data) == 0)
-		return (ft_printf_fd(2, "Error: Map must be surrounded by 1.\n"),
-			0);*/
+	/*	if (check_walls(data) == 0)
+			return (ft_printf_fd(2, "Error: Map must be surrounded by 1.\n"),
+				0);*/
 	return (1);
 }
